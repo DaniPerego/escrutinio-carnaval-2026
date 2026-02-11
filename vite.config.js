e8b3c37ba6811c0a6733f4717437c06e9fba5e1f
DaniPerego <dani@desarrolloweb-pdl.com>
2026-02-10 22:06:36 -0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/escrutinio-carnaval-2026/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
